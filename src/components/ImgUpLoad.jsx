01b8be57b966323dbd684b3234c527a33296997f
Seonghwan Sim <hoanshim01@handong.ac.kr>
2024-07-24 00:57:57 +0900

    chore: header 생성

--- src/components/ImgUpLoad.jsx
+++ src/components/ImgUpLoad.jsx
@@ -1,49 +1,8 @@
-// import React, { useState } from "react";
-// import ImgNone from "../imgs/img_none.svg";
-// import "../styles/ImgUpLoad.css";
-
-// export default function ImgUpLoad({ onImageupload }) {
-//   const [imageSrc, setImageSrc] = useState(`${ImgNone}`);
-
-//   const encodeFileToBase64 = (fileBlob) => {
-//     const reader = new FileReader();
-
-//     reader.readAsDataURL(fileBlob);
-
-//     return new Promise((resolve) => {
-//       reader.onload = () => {
-//         setImageSrc(reader.result);
-
-//         resolve();
-//       };
-//     });
-//   };
-
-//   // console.log(imageSrc);
-
-//   return (
-//     <main className="img-container">
-//       <div className="img-preview">
-//         {imageSrc && (
-//           <img className="img-image" src={imageSrc} alt="preview-img" />
-//         )}
-//       </div>
-
-//       <input
-//         type="file"
-//         onChange={(e) => {
-//           encodeFileToBase64(e.target.files[0]);
-//         }}
-//       />
-//     </main>
-//   );
-// }
 import React, { useState } from "react";
 import ImgNone from "../imgs/img_none.svg";
 import "../styles/ImgUpLoad.css";
 
 export default function ImgUpLoad({ onImageUpload }) {
-  // 이름 변경: onImageUpload
   const [imageSrc, setImageSrc] = useState(`${ImgNone}`);
 
   const encodeFileToBase64 = (fileBlob) => {
