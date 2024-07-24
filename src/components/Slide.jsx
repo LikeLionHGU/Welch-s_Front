08b678eba33c5754efa14ab373869ee8625b4e4c
Seonghwan Sim <hoanshim01@handong.ac.kr>
2024-07-25 01:26:23 +0900

    feat: 슬라이드 변경

--- src/components/Slide.jsx
+++ src/components/Slide.jsx
@@ -1,84 +1,72 @@
-import Slider from "react-slick";
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { FreeMode, Autoplay } from "swiper/modules";
+import SwiperCore from "swiper";
+import "swiper/css";
+import "swiper/css/free-mode";
+import "swiper/css/autoplay";
 import React from "react";
 import "../styles/slide.css";
 
+SwiperCore.use([FreeMode, Autoplay]);
+
 export default function Slide() {
-  // 커스텀 이전 화살표 컴포넌트
-  const PrevArrow = (props) => {
-    const { className, style, onClick } = props;
-    return (
-      <div
-        className={className}
-        style={{
-          ...style,
-          // display: "block",
-          background: "gray",
-          borderRadius: "50px",
-          padding: "0",
-          paddingBottom: "-1px",
-          zIndex: 1,
-        }}
-        onClick={onClick}
-      />
-    );
-  };
-
-  // 커스텀 다음 화살표 컴포넌트
-  const NextArrow = (props) => {
-    const { className, style, onClick } = props;
-    return (
-      <div
-        className={className}
-        style={{
-          ...style,
-          // display: "block",
-          background: "gray",
-          zIndex: 1,
-          marginRight: "10px",
-        }}
-        onClick={onClick}
-      />
-    );
-  };
-
-  const settings = {
-    prevArrow: <PrevArrow />,
-    nextArrow: <NextArrow />,
-    dots: true, // 아래 점들
-    infinite: true, // 무한으로 돌것인가
-    speed: 500, // 넘어갈 때 속도
-    slidesToShow: 4, // 한번에 볼 수 있는 슬라이드 개수
-    slidesToScroll: 1, // 한번에 넘어가는 슬라이드 개수
-    arrows: true, // 양쪽 버튼
-    //   autoplay: false, // 자동으로 넘어가는가
-    //   autoplaySpeed: 0, // 이동하는 속도
-    //   cssEase: 'liner', // 이동 스타일
-  };
-
   return (
-    <div>
-      <Slider {...settings} className="slide-container">
-        <div>
-          <h3>1</h3>
-        </div>
-        <div>
-          <h3>2</h3>
-        </div>
-        <div>
-          <h3>3</h3>
-        </div>
-        <div>
-          <h3>4</h3>
-        </div>
-        <div>
-          <h3 style={{ backgroundColor: "#999999" }}>5</h3>
-        </div>
-        <div>
-          <h3>6</h3>
-        </div>
-      </Slider>
-    </div>
+    <Swiper
+      style={{ margin: "0", whiteSpace: "pre-wrap" }}
+      breakpoints={{
+        0: {
+          slidesPerView: 3.5,
+          spaceBetween: 0,
+        },
+        900: {
+          slidesPerView: 3.5,
+          spaceBetween: 0,
+        },
+      }}
+      // freeMode={true} // 자유로운가
+      //loop={true} // 무한 루프
+      speed={3000} // 슬라이드 이동 속도
+      // loopedSlides={1}
+      loopAdditionalSlides={true}
+      autoplay={{ delay: 3000, disableOnInteraction: false }} // 자동 이동
+      slidesOffsetBefore={65} // 슬라이드 출발 지점에 빈 공간
+      slideToClickedSlide={true} // 슬라이드 클릭 시 이동
+      className="swiper"
+    >
+      {ServiceData.map((item, index) => (
+        <SwiperSlide key={item.title}>
+          <div className="swiperslide">
+            <div
+              className="swiperslide1"
+              id="sliderbg"
+              style={{ backgroundImage: `url(${item.backgroundImg})` }}
+            ></div>
+            <div className="swiperslide2" id="sliderhoverbg"></div>
+            <div className="swiperslide3" id="slidertext">
+              <div>
+                <CardQuestion>{item.question}</CardQuestion>
+                <CardTitle>{item.title}</CardTitle>
+                <CardContent>{item.content}</CardContent>
+              </div>
+              <div>
+                <TipContainer>
+                  <img
+                    src={Tip}
+                    alt="logo"
+                    style={{
+                      width: "30px",
+                      height: "30px",
+                      paddingRight: "15px",
+                    }}
+                  />
+                  <div style={{ fontFamily: "Plaid" }}>TIP</div>
+                </TipContainer>
+                <CardTip>{item.tip}</CardTip>
+              </div>
+            </div>
+          </div>
+        </SwiperSlide>
+      ))}
+    </Swiper>
   );
 }
