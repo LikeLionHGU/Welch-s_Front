cc4e300789d23e0729a1a62d0a4a1757751b2fe3
Seonghwan Sim <hoanshim01@handong.ac.kr>
2024-07-26 01:51:49 +0900

    feat: 메인페이지에 슬라이드 적용

--- src/pages/Mainpage.jsx
+++ src/pages/Mainpage.jsx
@@ -2,38 +2,43 @@ import Header from "../components/Header";
 import Slide from "../components/Slide";
 import "../styles/mainpage.css";
 
-import PrevArrowImg from "../imgs/prevArrow.svg";
-import NextArrowImg from "../imgs/nextArrow.svg";
-
 export default function Mainpage() {
   return (
     <>
       <Header />
       <div className="main-container">
-        <div>
+        <div className="section">
           <div>베스트 책</div>
-          <Slide />
+          <div className="slide-container">
+            <Slide />
+          </div>
         </div>
-        <div>
-          <div>
+        <div className="section">
+          <div className="section-above">
             <div>함께 책 만들 작가 모집</div>
             <div>더보기</div>
           </div>
-          <Slide />
+          <div className="slide-container">
+            <Slide />
+          </div>
         </div>
-        <div>
-          <div>
+        <div className="section">
+          <div className="section-above">
             <div>완결된 책</div>
             <div>더보기</div>
           </div>
-          <Slide />
+          <div className="slide-container">
+            <Slide />
+          </div>
         </div>
-        <div>
-          <div>
+        <div className="section">
+          <div className="section-above">
             <div>진행 중인 책</div>
             <div>더보기</div>
           </div>
-          <Slide />
+          <div className="slide-container">
+            <Slide />
+          </div>
         </div>
       </div>
     </>
