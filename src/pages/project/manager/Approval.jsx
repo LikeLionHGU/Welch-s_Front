0086c5bfa4d7d21299de99b251ec5a01e679c4a9
Seonghwan Sim <hoanshim01@handong.ac.kr>
2024-08-02 20:35:20 +0900

    style: 구조 정리

--- src/pages/project/manager/Approval.jsx
+++ src/pages/project/manager/Approval.jsx
@@ -1,9 +1,15 @@
 // 관리자 승인/버전별 수정/편집 페이지
 import Write from "../../../components/Write";
+
+import "../../../styles/approval.css";
+
 export default function Approval() {
   return (
-    <>
+    <div className="approval-write">
+      {/* 참여자가 수정한 것 */}
+      <Write user={2} mode={2} />
+      {/* 원본 */}
       <Write user={2} mode={1} />
-    </>
+    </div>
   );
 }
