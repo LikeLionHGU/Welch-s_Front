b01e8ec46b5e49a94b7dbfbc6f1b04f3703fa171
Seonghwan Sim <hoanshim01@handong.ac.kr>
2024-08-05 04:26:59 +0900

    style: 순서 수정

--- src/pages/project/manager/Approval.jsx
+++ src/pages/project/manager/Approval.jsx
@@ -15,11 +15,10 @@ export default function Approval() {
 
   return (
     <div className="approval-write">
-
-      {/* 참여자가 수정한 것 */}
-      <Write user={2} mode={2} id={id} updatedId={updatedId} />
       {/* 원본 */}
       <Write user={2} mode={1} id={id} updatedId={updatedId} />
+      {/* 참여자가 수정한 것 */}
+      <Write user={2} mode={2} id={id} updatedId={updatedId} />
     </div>
   );
 }
